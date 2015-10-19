2800e9b0b70436897ba98faf0ae62fa393503e0c
Martin Sauve <martin.sauve@gmail.com>
2015-10-19 09:07:10 -0400

    ignore list

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,5 +1,2 @@
 echo $1
 curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o ROOT.war
-cd ..
-git commit -a -m 'version $1'
-git push
