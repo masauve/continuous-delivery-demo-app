ffc9eb87accf894c84234c4777a4c9bebe401001
Martin Sauve <martin.sauve@gmail.com>
2015-10-19 08:32:45 -0400

    version $1

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,4 +1,5 @@
 echo $1
 curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o ROOT.war
+cd ..
 git commit -a -m 'version $1'
 git push
