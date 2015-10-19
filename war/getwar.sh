aba9f08a92dea495beced08aa8eb1a35630e38b7
Martin Sauve <martin.sauve@gmail.com>
2015-10-19 14:18:54 -0400

    t

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,2 +1,2 @@
 echo $1
-curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o /opt/demo/openshift-origin-v3/continuous-delivery-demo-app/war/ticket-monster-$1.war
+curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o /opt/demo/openshift-origin-v3/continuous-delivery-demo-app/war/ROOT.war
