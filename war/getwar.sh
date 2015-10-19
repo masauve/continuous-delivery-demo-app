eb04ec344af7d0c6a225bf555968f0a324ee8659
Martin Sauve <martin.sauve@gmail.com>
2015-10-19 14:15:09 -0400

    t

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,2 +1,3 @@
 echo $1
-curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o ROOT.war
+curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o ticket-monster-$1.war
+cp ticket-monster-$1.war ROOT.war
