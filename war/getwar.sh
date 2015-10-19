608f65abe21b04b10346038c8645f9ddbc31ec85
Martin Sauve <martin.sauve@gmail.com>
2015-10-19 08:31:22 -0400

    version $1

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,1 +1,4 @@
-curl http://192.168.59.104:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/2.6.97/ticket-monster-2.6.97.war -o ROOT.war
+echo $1
+curl http://nexus:8081/nexus/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o ROOT.war
+git commit -a -m 'version $1'
+git push
