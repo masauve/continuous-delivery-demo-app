e1b34bb2e311cf4acf00e3c41966257ceb9377ef
Martin Sauve <martin.sauve@gmail.com>
2015-11-06 13:42:02 -0500

    new version $1

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,2 +1,5 @@
 echo $1
 curl http://nexus.vagrant.f8/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o /opt/demo/openshift-origin-v3/continuous-delivery-demo-app/war/ROOT.war
+
+git commit -a -m 'new version $1'
+git push
