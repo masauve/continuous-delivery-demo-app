d6d4a88b1d79d3936c42f7a0f91038c0a5295bae
Martin Sauve <martin.sauve@gmail.com>
2015-11-06 13:58:52 -0500

    test

--- war/getwar.sh
+++ war/getwar.sh
@@ -1,5 +1,6 @@
 echo $1
+git checkout
 curl http://nexus.vagrant.f8/service/local/repositories/releases/content/org/jboss/examples/ticket-monster/$1/ticket-monster-$1.war -o /opt/demo/openshift-origin-v3/continuous-delivery-demo-app/war/ROOT.war
 
-git commit -a -m 'new version $1'
+git commit -a -m 'test'
 git push
